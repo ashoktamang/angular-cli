f2614148a584ab96f091e415026e66151d0408fb
Ashok Tamang <ashoktamang52@gmail.com>
2016-06-30 20:31:52 -0700

    wip: final version of module-resolve

--- addon/ng2/utilities/module-resolve.ts
+++ addon/ng2/utilities/module-resolve.ts
@@ -1,4 +1,4 @@
- 'use strict';
+'use strict';
 
 import * as dependentFilesUtils from './get-dependent-files';
 import * as path from 'path';
@@ -6,156 +6,258 @@ import * as Promise from 'ember-cli/lib/ext/promise';
 import * as fs from 'fs';
 import {InsertChange, RemoveChange, ReplaceChange} from './change';
 
-const rootPath = 'src/app';
-const readFile = Promise.denodeify(fs.readFile);
-const writeFile = Promise.denodeify(fs.writeFile);
+/** @const The root directory of Angular Project. */
+const ROOT_PATH = 'src/app';
 
+declare type ChangeMap {
+  [key: string]: ChangeMap: string;
+}
+
+/** 
+ * @class Resolves the module of dependent files when the file is moved. 
+ */
 export class ModuleResolve {
 
+  /** @member Description of the module resolution. */
   const description: string;
 
-  constructor(public oldFilePath: string, public newFilePath: string) {}
+  /** @constructs */
+  constructor(public oldFilePath: string, public newFilePath: string) {
+    let component = path.basename(oldFilePath);
+    this.description = `Promoted ${component} from ${oldFilePath} to ${newFilePath}`;
+  }
 
-  public diffPaths(): string {
-    let oldDir = path.dirname(this.oldFilePath);
-    let newDir = path.dirname(this.newFilePath);
+  /**
+   * @return {MapType} a map of necessary changes to make for module resolution.
+   * @example If a file is moved one level up the directory tree, then the map will contain:
+   * {'text': changeText, 'direction': 'MoveUp', 'dirLevel': 1}
+   */
+   diffPaths(oldFilePath: string, newFilePath: string): ChangeMap {
+
+    /** Check whether old andn new path have same parent directory. */
+    let oldDir: string = path.dirname(this.oldFilePath);
+    let newDir: string = path.dirname(this.newFilePath);
 
     if (oldDir === newDir) {
-      throw new Error('You are moving the component to the same place, Duh!');
+      return {
+        'text': path.basename(this.newFilePath),
+        'direction': 'NoMove',
+        'dirLevel': 0
+      };
     }
 
-    let oldDir = oldDir.split(path.sep);
-    let newDir = newDir.split(path.sep);
-    let parsedRootPath = rootPath.split(path.sep);
+    /** Parsed into an array to check the length. */
+    oldDir = oldDir.split(path.sep);
+    newDir = newDir.split(path.sep);
 
+    /** Check whether move up or down the directory tree. */
     if (newDir.length > oldDir.length) {
-      let toAdd: string[] = newDir.filter(i => {
-        if (newDir.indexOf(i) > parsedRootPath.length - 1) {
-          return i;
-        };
+      let changeText: string[] = newDir.filter(i => {
+        return oldDir.indexOf(i) === -1;
       });
-      console.log('toAdd', toAdd);
-			let temp = '';
-			toAdd.forEach(pathSegment => {
-				temp = temp.concat('/', pathSegment);
-			};
-			return {
-				'text': path.normalize(temp),
-				'operation': 'toAdd'
-			};
-    } else {
-      let toRemove: string[] = oldDir.filter(i => {
-        return rootPath.indexOf(i) === -1;
-      });
-      console.log(toRemove);
-        // i => {
-        // if (oldDir.indexOf(i) > parsedRootPath.length - 1) {
-        //   return i;
-        // };
-      //});
-			let temp = '';
-			toRemove.forEach(pathSegment => {
-				temp = temp.concat('/', pathSegment);
-			};
-			return {
-				'text': path.normalize(temp),
-				'operation': 'toRemove'
+      let temp = '';
+      changeText.forEach(pathSegment => {
+        temp = temp.concat('/', pathSegment);
       };
-  
-  };
+      return {
+        'text': path.normalize(temp),
+        'direction': 'MoveDown',
+        'dirLevel': changeText.length
+      };
+    } else {
+      let changeText: string[] = oldDir.filter(i => {
+        return newDir.indexOf(i) === -1;
+      });
+      let temp = '';
+      changeText.forEach(pathSegment => {
+        temp = temp.concat('/', pathSegment);
+      };
+      return {
+        'text': path.normalize(temp),
+        'direction': 'MoveUp',
+        'dirLevel': changeText.length
+      };
+    };
   };
 
-  public parseModuleSpecifiers(filePath: string): any {
+  /**
+   * Changes are applied from the bottom of a file to the top.
+   * An array of Change instances are sorted based upon the order, 
+   * then apply() method is called sequentially.
+   * 
+   * @param changes {Change []} 
+   * @return Promise after all apply() method of Change class is called sequentially.
+   */
+  public sortedChangePromise(changes: Change[]): Promise {
+    changes = changes.sort((a, b) => return b.order - a.order; );
+    let changePromise = new Promise((resolve, reject) => {return resolve(); );
+    changes.forEach(change => {
+      changePromise = changePromise.then(() => change.apply());
+    });
+    return changePromise;
+  }
+
+  /**
+   * @return {Promise} an array of Promise returned by Change instance
+   */
+  public parseModuleSpecifiers(filePath: string): Promise<any> {
     let fileName = '';
+
     fs.readdirSync(path.join(this.oldFilePath)).forEach(file => {
-			if (path.extname(file) == '.ts') {
+    if (path.extname(file) == '.ts') {
         if (path.extname(path.basename(file, '.ts')) === '.component') {
           fileName = file;
         }
       }
-		});
-		fileName = path.join(rootPath, this.oldFilePath, fileName);
-		let files = dependentFilesUtils.getDependentFiles(fileName, rootPath);
-    // return this.ashok(files);
+    });
+    fileName = path.join(ROOT_PATH, this.oldFilePath, fileName);
+    let files = dependentFilesUtils.getDependentFiles(fileName, ROOT_PATH);
     if (Object.getOwnPropertyNames(files).length > 0) {
-      let change = this.diffPaths();
-      console.log(change);
-      
-      if (change.operation === 'toRemove') {
+      let change = this.diffPaths(this.oldFilePath, this.newFilePath);
+      if (change.direction === 'MoveUp') {
          let changes = [];
-        
+         let oldDir = path.basename(this.oldFilePath);
         for (let file of Object.keys(files)) {
+          // When change.text is not in the ModuleSpecifier
           if (files[file].specifier.indexOf(change.text) === -1) {
-            console.log('please go here');
-            changes.push[new InsertChange(file, files[file].pos + 2, '.')];
+            // Checking if a file and its dependent file are under same parent directory.
+            // For instance, bar/baz/ and bar/bar.component.ts has same parent 'bar'.
+            if (path.dirname(file) === path.dirname(this.oldFilePath)) {
+              let position = files[file].end - files[file].specifier.length;
+              let a = new InsertChange(file, position, '.');
+              changes.push(a);
+            } else {
+              let position = files[file].end - `/${oldDir}`.length;
+              let addText = '../'.repeat(change.dirLevel);
+              let a = new InsertChange(file, position, addText);
+              changes.push(a);
+            }
           } else {
-            changes.push[new RemoveChange(file, files[file].pos + 4, change.text)];
+            let position = files[file].end - `${change.text}/${oldDir}'`.length;
+            let a = new RemoveChange(file, position, change.text);
+            changes.push(a);
           }
         }
-
-        let pathb = 'src/app/bar/bar.component.ts';
-        a = new InsertChange(pathb, files[pathb].pos + 2, '.');
-        return a.apply();
+        changes = changes.map(change => change.apply());
+        return Promise.all(changes);
       }
-      if (change.operation === 'toAdd') {
-        let a;
-        console.log(files);
+
+      if (change.direction === 'MoveDown') {
+        let changes = [];
         let oldDir = path.basename(this.oldFilePath);
         for (let file of Object.keys(files)) {
-          // if (path.basename(path.dirname(file)) === path.basename(path.dirname(this.newFilePath))) {
-          //   let position = files[file].pos + 2;
-          //   a = new RemoveChange(file, position, '.');
-          // } else {
-          //   let position = files[file].end - oldDir.length - 1 - 1;
-          //   a = new InsertChange(file, position, change.text);
-          // }
-          let position = files[file].end - oldDir.length - 1 - 1;
-          a = new InsertChange(file, position, change.text);
+          // Check whether newFilePath and the dependent file have the same parent directory
+          if (path.basename(path.dirname(file)) === path.basename(path.dirname(this.newFilePath))) {
+            let position = files[file].end - files[file].specifier.length;
+            let a = new RemoveChange(file, position - 1, '.');
+            changes.push(a);
+          } else {
+            let position = files[file].end - `/${oldDir}'`.length;
+            let a = new InsertChange(file, position, change.text);
+            changes.push(a);
+          }
         }
-        return a.apply();
+        changes = changes.map(change => change.apply());
+        return Promise.all(changes);
+      }
+
+      if (change.direction === 'NoMove') {
+        let changes = [];
+        let oldDir = path.basename(this.oldFilePath);
+        for (let file of Object.keys(files)) {
+          let position = files[file].end - `${oldDir}'`.length;
+          let a = new ReplaceChange(file, position, oldDir, change.text);
+          changes.push(a);
+        }
+        changes = changes.map(change => change.apply());
+        return Promise.all(changes);
       }
     } else {
-      return 'No dependent files';
+      return Promise.resolve('No dependent files to resolve.');
     }
-  }
-  // }
+  };
 
-//   public ashok(files) : Promise<any>{
-  
-//   if (Object.getOwnPropertyNames(files).length > 0) {
-//       let change = this.diffPaths();
-      
-//       if (change.operation === 'toRemove') {
-//         let a;
-        
-//         for (let file of Object.keys(files)) {
-//           if (files[file].specifier.indexOf(change.text) === -1) {
-//             console.log('please go here');
-//             a = new InsertChange(file, files[file].pos + 2, '.');
-//           } else {
-//             a = new RemoveChange(file, files[file].pos + 4, change.text);
-//           }
-//         }
-//         return a.apply();
-//       }
-//       if (change.operation === 'toAdd') {
-//         let a;
-//         console.log(files);
-//         let oldDir = path.basename(this.oldFilePath);
-//         for (let file of Object.keys(files)) {
-//           if (path.basename(path.dirname(file)) === path.basename(path.dirname(this.newFilePath))) {
-//             let position = files[file].pos + 2;
-//             a = new RemoveChange(file, position, '.');
-//           } else {
-//             let position = files[file].end - oldDir.length - 1 - 1;
-//             a = new InsertChange(file, position, change.text);
-//           }
-//         }
-//         return a.apply();
-//       }
-//     } else {
-//       return Promise.reject('No dependent files');
-//     }
-//   }
-// }
+  public resolveFile(): Promise<void> {
+    let fileName;
+    let filePath;
+    let oldPathArray;
+    let newPathArray;
+    if (path.extname(this.oldFilePath) === '.ts') {
+      fileName = this.oldFilePath;
+      filePath = path.join(fileName);
+      oldPathArray = path.dirname(filePath).split(path.sep);
+      newPathArray = path.dirname(this.newFilePath).split(path.sep);
+    } else {
+      fs.readdirSync(path.join(this.oldFilePath)).forEach(file => {
+        if (path.extname(file) == '.ts') {
+          if (path.extname(path.basename(file, '.ts')) === '.component') {
+            fileName = file;
+          }
+        }
+        filePath = path.join(this.oldFilePath, fileName);
+      });
+      oldPathArray = this.oldFilePath.split(path.sep);
+      newPathArray = this.newFilePath.split(path.sep);
+    }
+
+    let tsFile = dependentFilesUtils.createtsSourceFile(filePath);
+
+    let modules: string[] = [];
+    dependentFilesUtils.getModuleSpecifier(tsFile, modules);
+
+    // Instantiate an empty array for all Changes instance
+    let changes = [];
+
+    modules.forEach(specifier => {
+      let specifierArray = specifier.specifier.split(path.sep);
+      // Change involves replacing whole string of module specifier less the module name
+      let replaceText = '';
+      if (specifierArray.length > 2 || specifierArray[0] == '..') {
+        let parentDirSyntaxCounter = 0;
+        // Check the frequency of '..' in the array.
+        // Later, the frequency is used to determine the absolute path of a given module.
+        specifierArray.forEach(dirName => {
+          if (dirName === '..') {
+            parentDirSyntaxCounter++;
+          }
+        });
+        let parsedPath = oldPathArray.slice(0, oldPathArray.length - parentDirSyntaxCounter);
+        specifierArray.forEach(dirName => {
+          if (dirName !== '..') {
+            parsedPath.push(dirName);
+          }
+        });
+        let rootDirLevel = (ROOT_PATH.split(path.sep)).length; // To omit 'src/app' in replaceText
+        let  tempNewPathArray = newPathArray.slice(rootDirLevel, newPathArray.length);
+        let sameDirCounter = 0;
+        let replaceTextArray = [];
+        let tempRootLevel = rootDirLevel;
+        // Checks if the file is moved under the same directory 
+        // from where it is importing its module.
+        tempNewPathArray.forEach(dirName => {
+          if (dirName === parsedPath[tempRootLevel]) {
+            replaceTextArray = ['.'];
+            sameDirCounter++;
+          } else {
+            replaceTextArray.push('..');
+          }
+        });
+        let tempParsedArray = parsedPath.slice(rootDirLevel + sameDirCounter,
+                                               parsedPath.length - 1);
+        replaceTextArray = replaceTextArray.concat(tempParsedArray);
+        replaceTextArray.forEach(dirName => replaceText = path.join(replaceText, dirName));
+      }
+      let moduleName = specifierArray[specifierArray.length - 1];
+
+      // ReplaceChange is being applied here
+      let position = specifier.end - specifier.specifier.length;
+      // The text to be replaced will always be the 
+      // whole string of module specifier less the module name
+      let upToModuleRange = (specifier.specifier.length - 1) - moduleName.length;
+      let toBeReplacedText = specifier.specifier.slice(0, upToModuleRange);
+
+      let replace = new ReplaceChange(filePath, position - 1, toBeReplacedText, replaceText);
+      changes.push(replace);
+    });
+    return this.sortedChangePromise(changes);
 }
--- tests/acceptance/module-resolve.spec.ts
+++ tests/acceptance/module-resolve.spec.ts
@@ -1,16 +1,11 @@
 'use strict';
 
 import * as mockFs from 'mock-fs';
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 
 import * as path from 'path';
 import { ModuleResolve } from '../../addon/ng2/utilities/module-resolve';
-import * as Promise from 'ember-cli/lib/ext/promise';
 import * as dependentFilesUtils from '../../addon/ng2/utilities/get-dependent-files';
-import * as fs from 'fs';
-
-
-const readFile = Promise.denodeify(fs.readFile);
 
 describe('ModuleResolve', () => {
   let rootPath = 'src/app';
@@ -19,17 +14,32 @@ describe('ModuleResolve', () => {
     let mockDrive = {
       'src/app': {
         'foo': {
-          'foo.component.ts': 'import * from "../bar/baz"'
+          'foo.component.ts': `import * from "../bar/baz"`
         },
         'bar': {
           'baz': {
-            'baz.component.ts': 'import * from "../bar"'
+            'baz.component.ts': `import * from "../../bar"
+                                 import * from '../../foo-baz/qux/quux/foobar'
+                                 `
           },
           'bar.component.ts': `import * from './baz'
                                import * from '../foo'`
         },
         'foo-baz': {
-          'no-module.component.ts': ''
+          'qux': {
+            'quux': {
+              'foobar': {
+                'foobar.component.ts': `import * from "../../../../foo"
+                                        import * from '../fooqux'
+                                        `
+              },
+              'fooqux': {
+                'fooqux.component.ts': 'import * from                   "../foobar"'
+              }
+            }
+          }
+          'no-module.component.ts': '',
+          'foo-baz.component.ts': 'import * from \n"../foo"\n'
         },
         'empty-dir': {}
       }
@@ -40,23 +50,53 @@ describe('ModuleResolve', () => {
     mockFs.restore();
   });
 
-  it.only('moving up the dir', () => {
+  it('moving one level up the directory tree', () => {
       let oldFilePath = path.join(rootPath, 'bar/baz');
       let newFilePath = path.join(rootPath, 'baz');
       let resolve = new ModuleResolve(oldFilePath, newFilePath);
       return resolve.parseModuleSpecifiers(rootPath)
       .then(() => {
-          let contents = [];
-          let tsFile = dependentFilesUtils.createtsSourceFile('src/app/bar/bar.component.ts');
-          dependentFilesUtils.getModuleSpecifier(tsFile, contents);
-          console.log(fs.readFileSync('src/app/bar/bar.component.ts', 'utf8'));
-          expect(contents[0].specifier).to.equal('../baz');
-          expect(contents[1].specifier).to.equal('../foo');
+          let contentsBar = [];
+          let tsFileBar = dependentFilesUtils.createtsSourceFile('src/app/bar/bar.component.ts');
+          dependentFilesUtils.getModuleSpecifier(tsFileBar, contentsBar);
+          expect(contentsBar[0].specifier).to.equal('../baz');
+
+          let contentsFoo = [];
+          let tsFileFoo = dependentFilesUtils.createtsSourceFile('src/app/foo/foo.component.ts');
+          dependentFilesUtils.getModuleSpecifier(tsFileFoo, contentsFoo);
+          expect(contentsFoo[0].specifier).to.equal('../baz');
       });
   });
-  it('moving down the dir', () => {
+  it('moving one level down the directory tree', () => {
      let oldFilePath = path.join(rootPath, 'foo');
      let newFilePath = path.join(rootPath, 'bar/foo');
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.parseModuleSpecifiers(rootPath)
+     .then(() => {
+        let contents = [];
+        let tsFile = dependentFilesUtils.createtsSourceFile('src/app/bar/bar.component.ts');
+        dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+        expect(contents[1].specifier).to.equal('./foo');
+
+        let contentsFooBar = [];
+        let sourcePath = 'src/app/foo-baz/qux/quux/foobar/foobar.component.ts';
+        let tsFileFooBar = dependentFilesUtils.createtsSourceFile(sourcePath);
+        dependentFilesUtils.getModuleSpecifier(tsFileFooBar, contentsFooBar);
+        expect(contentsFooBar[0].specifier).to.equal('../../../../bar/foo');
+     });
+  });
+  it('no dependent files', () => {
+     let oldFilePath = path.join(rootPath, 'foo-baz');
+     let newFilePath = path.join(rootPath, 'bar/foo-baz');
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.parseModuleSpecifiers(rootPath).then(content => {
+         expect(content).to.equal('No dependent files to resolve.');
+
+     });
+  });
+  it('moving three levels down the directory tree', () => {
+     let oldFilePath = path.join(rootPath, 'foo');
+     let newFilePath = path.join(rootPath, '/foo-baz/qux/quux/foo');
      let resolve = new ModuleResolve(oldFilePath, newFilePath);
      return resolve.parseModuleSpecifiers(rootPath)
      .then(() => {
@@ -64,22 +104,119 @@ describe('ModuleResolve', () => {
          let tsFile = dependentFilesUtils.createtsSourceFile('src/app/bar/bar.component.ts');
          dependentFilesUtils.getModuleSpecifier(tsFile, contents);
          expect(contents[0].specifier).to.equal('./baz');
-         expect(contents[1].specifier).to.equal('./foo');
+         expect(contents[1].specifier).to.equal('../foo-baz/qux/quux/foo');
      });
   });
-  it('no dependent files', () => {
-     let oldFilePath = path.join(rootPath, 'foo-baz');
-     let newFilePath = path.join(rootPath, 'bar/foo-baz');
+  it('moving in depeer level of directory tree', () => {
+     let oldFilePath = path.join(rootPath, 'foo-baz/qux/quux/fooqux');
+     let newFilePath = path.join(rootPath, 'foo-baz/qux/fooqux';
      let resolve = new ModuleResolve(oldFilePath, newFilePath);
-    //  console.log(resolve.parseModuleSpecifiers(rootPath));
-  });
-  it('moving down the dir', () => {
-     let oldFilePath = path.join(rootPath, 'foo');
-     let newFilePath = path.join(rootPath, 'bar/foo');
-     let resolve = new ModuleResolve(oldFilePath, newFilePath);
-     resolve.parseModuleSpecifiers(rootPath)
+     return resolve.parseModuleSpecifiers(rootPath)
      .then(() => {
-        //  console.log(fs.readFileSync(path.normalize('src/app/bar/bar.component.ts'), 'utf8'));
+         let contents = [];
+         let path = 'src/app/foo-baz/qux/quux/foobar/foobar.component.ts';
+         let tsFile = dependentFilesUtils.createtsSourceFile(path);
+         dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+         expect(contents[1].specifier).to.equal('../../fooqux');
+     });
+  });
+  it('multiple spaces before moduleSpecifier', () => {
+     let oldFilePath = path.join(rootPath, 'foo-baz/qux/quux/foobar');
+     let newFilePath = path.join(rootPath, 'foo-baz/qux/foobar';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.parseModuleSpecifiers(rootPath)
+     .then(() => {
+         let contents = [];
+         let path = 'src/app/foo-baz/qux/quux/fooqux/fooqux.component.ts';
+         let tsFile = dependentFilesUtils.createtsSourceFile(path);
+         dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+         expect(contents[0].specifier).to.equal('../../foobar');
+     });
+  });
+  it('new line in import statement', () => {
+     let oldFilePath = path.join(rootPath, 'foo');
+     let newFilePath = path.join(rootPath, 'bar/foo';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.parseModuleSpecifiers(rootPath)
+     .then(() => {
+         let contents = [];
+         let path = 'src/app/foo-baz/foo-baz.component.ts';
+         let tsFile = dependentFilesUtils.createtsSourceFile(path);
+         dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+         expect(contents[0].specifier).to.equal('../bar/foo');
+     });
+  });
+  it('renaming the component file', () => {
+     let oldFilePath = path.join(rootPath, 'foo');
+     let newFilePath = path.join(rootPath, 'newFoo';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.parseModuleSpecifiers(rootPath)
+     .then(() => {
+         let contentsBar = [];
+         let pathBar = 'src/app/bar/bar.component.ts';
+         let tsFileBar = dependentFilesUtils.createtsSourceFile(pathBar);
+         dependentFilesUtils.getModuleSpecifier(tsFileBar, contentsBar);
+         expect(contentsBar[1].specifier).to.equal('../newFoo');
+
+         let contentsFooBar = [];
+         let pathFooBar = 'src/app/foo-baz/qux/quux/foobar/foobar.component.ts';
+         let tsFileFooBar = dependentFilesUtils.createtsSourceFile(pathFooBar);
+         dependentFilesUtils.getModuleSpecifier(tsFileFooBar, contentsFooBar);
+         expect(contentsFooBar[0].specifier).to.equal('../../../../newFoo');
+     });
+  });
+  it('resolve oneself, moving down', () => {
+     let oldFilePath = path.join(rootPath, 'foo');
+     let newFilePath = path.join(rootPath, 'bar/foo';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.resolveFile(rootPath)
+     .then(() => {
+        let contents = [];
+        let path = 'src/app/foo/foo.component.ts';
+        let tsFile = dependentFilesUtils.createtsSourceFile(path);
+        dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+        expect(contents[0].specifier).to.equal('../baz');
+     });
+  });
+  it('resolve oneself, moving up', () => {
+     let oldFilePath = path.join(rootPath, 'bar/baz');
+     let newFilePath = path.join(rootPath, 'baz';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.resolveFile(rootPath)
+     .then(() => {
+        let contents = [];
+        let path = 'src/app/bar/baz/baz.component.ts';
+        let tsFile = dependentFilesUtils.createtsSourceFile(path);
+        dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+        expect(contents[0].specifier).to.equal('../bar');
+        expect(contents[1].specifier).to.equal('../foo-baz/qux/quux/foobar');
+     });
+  });
+  it('resolve oneself, moving up [nested]', () => {
+     let oldFilePath = path.join(rootPath, 'foo-baz/qux/quux/foobar');
+     let newFilePath = path.join(rootPath, 'foobar';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.resolveFile(rootPath)
+     .then(() => {
+        let contents = [];
+        let path = 'src/app/foo-baz/qux/quux/foobar/foobar.component.ts';
+        let tsFile = dependentFilesUtils.createtsSourceFile(path);
+        dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+        expect(contents[0].specifier).to.equal('../foo');
+        expect(contents[1].specifier).to.equal('../foo-baz/qux/quux/fooqux');
+     });
+  });
+  it('resolve oneself, when module is in the same directory', () => {
+     let oldFilePath = path.join(rootPath, 'foo/foo.component.ts');
+     let newFilePath = path.join(rootPath, 'bar/foo.component.ts';
+     let resolve = new ModuleResolve(oldFilePath, newFilePath);
+     return resolve.resolveFile(rootPath)
+     .then(() => {
+        let contents = [];
+        let path = 'src/app/foo/foo.component.ts';
+        let tsFile = dependentFilesUtils.createtsSourceFile(path);
+        dependentFilesUtils.getModuleSpecifier(tsFile, contents);
+        expect(contents[0].specifier).to.equal('./baz');
      });
   });
 });
